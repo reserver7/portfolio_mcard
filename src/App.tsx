ef216db074780b350f42d7f8c19e41d62073f237
reserver7 <reserver7@gmail.com>
2024-04-09 13:31:02 +0900

    feat: 프로젝트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Text from '@shared/Text'
+import Button from '@shared/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Text typography="t1" display="block" color="red">
+        1
+      </Text>
+      <Text typography="t2" color="blue">
+        2
+      </Text>
+      <Text typography="t3">3</Text>
+      <Text typography="t4">4</Text>
+      <Text>5</Text>
+
+      <div style={{ height: 10, width: '100%', backgroundColor: '#efefef' }}>
+        <Button>클릭해주세요</Button>
+        <Button color="success">클릭해주세요</Button>
+        <Button color="error">클릭해주세요</Button>
+        <Button color="success" weak={true}>
+          클릭해주세요
+        </Button>
+        <Button color="error" weak={true}>
+          클릭해주세요
+        </Button>
+        <Button full={true}>클릭해주세요</Button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,45 @@
+import {
+  ButtonColor,
+  buttonColorMap,
+  ButtonSize,
+  buttonSizeMap,
+  buttonWeakMap,
+} from '@/styles/button'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+interface ButtonProps {
+  color?: ButtonColor
+  size?: ButtonSize
+  weak?: boolean
+  full?: boolean
+  disabled?: boolean
+}
+
+const Button = styled.button<ButtonProps>(
+  {
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    borderRadius: '6px',
+  },
+  ({ color = 'primary', weak }) =>
+    weak ? buttonWeakMap[color] : buttonColorMap[color],
+  ({ size = 'small' }) => buttonSizeMap[size],
+  ({ full }) =>
+    full
+      ? css`
+          display: block;
+          width: 100%;
+          border-radius: 0;
+        `
+      : undefined,
+  ({ disabled }) =>
+    disabled
+      ? css`
+          opacity: 0.26;
+          cursor: initial;
+        `
+      : undefined,
+)
+
+export default Button

30a8df030e08c74c8ebd89ec5c5756ce484d8398
reserver7 <reserver7@gmail.com>
2024-04-09 16:07:50 +0900

    feat: 공통 컴포넌트 구성 Input, Alert, Flex Box, Colors, Text, Button

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Global } from '@emotion/react'
 import globalStyles from './styles/globalStyles'
+import { AlertContextProvider } from './contexts/AlertContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Global styles={globalStyles} />
-    <App />
+    <AlertContextProvider>
+      <App />
+    </AlertContextProvider>
   </React.StrictMode>,
 )
 

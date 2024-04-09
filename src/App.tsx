c5b4f62f1b336a276ece1bf40246d58b1b204a29
reserver7 <reserver7@gmail.com>
2024-04-09 16:50:58 +0900

    feat: card list 데이터 firebase에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,15 @@
-import './App.css'
-import Text from '@shared/Text'
-import Button from '@shared/Button'
-import Input from '@shared/Input'
-import TextField from '@shared/TextField'
-import Alert from '@shared/Alert'
-import { useAlertContext } from '@contexts/AlertContext'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import HomePage from '@pages/Home'
+import TestPage from '@pages/Test'
 
 function App() {
-  const { open } = useAlertContext()
-
   return (
-    <div>
-      <Text typography="t1" display="block" color="red">
-        1
-      </Text>
-      <Text typography="t2" color="blue">
-        2
-      </Text>
-      <Text typography="t3">3</Text>
-      <Text typography="t4">4</Text>
-      <Text>5</Text>
-
-      <div style={{ height: 10, width: '100%', backgroundColor: '#efefef' }}>
-        <Button>클릭해주세요</Button>
-        <Button color="success">클릭해주세요</Button>
-        <Button color="error">클릭해주세요</Button>
-        <Button color="success" weak={true}>
-          클릭해주세요
-        </Button>
-        <Button color="error" weak={true}>
-          클릭해주세요
-        </Button>
-        <Button full={true}>클릭해주세요</Button>
-      </div>
-
-      <Input placeholder="로그인" aria-invalid={false} />
-      <Input />
-
-      <TextField label="아이디" />
-      <TextField label="패스워드" hasError={true} />
-
-      {/* <Alert open={true} title="알럿" onButtonClick={() => {}} /> */}
-
-      <Button
-        onClick={() => {
-          open({
-            title: '카드신청완료',
-            description: '내역페이지에서 확인해주세요.',
-            onButtonClick: () => {
-              //
-            },
-          })
-        }}
-      >
-        Alert 오픈
-      </Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/test" Component={TestPage} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

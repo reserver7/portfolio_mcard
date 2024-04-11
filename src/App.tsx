68159a4a5ccdda92a422ef3a5ffea6c13a26862d
reserver7 <reserver7@gmail.com>
2024-04-11 17:32:35 +0900

    feat: card list 상세페이지 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import HomePage from '@pages/Home'
 import TestPage from '@pages/Test'
+import CardPage from '@pages/Card'
+import ScrollToTop from '@shared/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/card/:id" Component={CardPage} />
         <Route path="/test" Component={TestPage} />
       </Routes>
     </BrowserRouter>

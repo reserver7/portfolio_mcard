89411e30abd42f80aec0b690183fd3a2592259c3
reserver7 <reserver7@gmail.com>
2024-04-11 14:08:55 +0900

    feat: card list 스와이프 & 스크롤 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import HomePage from '@pages/Home'
 import TestPage from '@pages/Test'
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,16 @@
-import { getAdBanners } from '@/remote/adBanner'
-import { getCards } from '@/remote/card'
 import Top from '@shared/Top'
-import React, { useEffect } from 'react'
+import AdBanners from '@components/home/AdBanners'
+import CardList from '@components/home/CardList'
 
-const HomePage = () => {
-  useEffect(() => {
-    getCards().then((response) => {
-      console.log('response', response)
-    })
-
-    getAdBanners().then((response) => {
-      console.log('response', response)
-    })
-  }, [])
-
+function HomePage() {
   return (
     <div>
       <Top
         title="혜택 좋은 카드"
         subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요"
       />
+      <AdBanners />
+      <CardList />
     </div>
   )
 }

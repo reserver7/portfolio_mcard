03eecd023e9078302b1e7a251ac35753ec11def3
reserver7 <reserver7@gmail.com>
2024-04-15 14:46:39 +0900

    feat: 카드신청 예외처리 & localStorage 저장

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -7,12 +7,46 @@ import { updateApplyCard } from '@/remote/apply'
 import { APPLY_STATUS } from '@/models/apply'
 import useUser from '@/hooks/auth/useUser'
 import { useNavigate, useParams } from 'react-router-dom'
+import useAppliedCard from '@/components/apply/hooks/useAppliedCard'
+import { useAlertContext } from '@contexts/AlertContext'
+import FullPageLoader from '@/components/shared/FullPageLoader'
 
 function ApplyPage() {
   const navigate = useNavigate()
   const [readyToPoll, setReadyToPoll] = useState(false)
   const user = useUser()
   const { id } = useParams() as { id: string }
+  const { open } = useAlertContext()
+
+  const storageKey = `applied-${user?.uid}-${id}`
+
+  const { data } = useAppliedCard({
+    userId: user?.uid as string,
+    cardId: id,
+    options: {
+      onSuccess: (applied) => {
+        if (applied == null) {
+          return
+        }
+
+        if (applied.status === APPLY_STATUS.COMPLETE) {
+          open({
+            title: '이미 발급이 완료된 카드입니다.',
+            onButtonClick: () => {
+              window.history.back()
+              localStorage.removeItem(storageKey)
+            },
+          })
+
+          return
+        }
+
+        setReadyToPoll(true)
+      },
+      onError: () => {},
+      suspense: true,
+    },
+  })
 
   usePollApplyStatus({
     onSuccess: async () => {
@@ -53,8 +87,12 @@ function ApplyPage() {
     },
   })
 
+  if (data != null && data.status === APPLY_STATUS.COMPLETE) {
+    return null
+  }
+
   if (readyToPoll || 카드를신청중인가) {
-    return <div>Loading...</div>
+    return <FullPageLoader message="카드를 신청중입니다" />
   }
 
   return <Apply onSubmit={mutate} />
